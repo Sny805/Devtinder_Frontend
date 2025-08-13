4a0c41f3e99a953abc7c7b3d65ad24672a060c77
Sunny Kumar <sunnymahto9824@gmail.com>
2025-08-13 23:24:24 +0530

    dynamic path added to web socket

--- src/utills/socket.js
+++ src/utills/socket.js
@@ -1,13 +1,15 @@
-import io from "socket.io-client"
-import { BASE_URL } from "./constants"
-
+import io from "socket.io-client";
+import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
     if (location.hostname === "localhost") {
-        return io(BASE_URL)
+        return io(BASE_URL, {
+            transports: ["websocket"],
+        });
+    } else {
+        return io("https://namaste-node-js-rhsn.onrender.com", {
+            transports: ["websocket"],
+            path: "/socket.io",
+        });
     }
-    else {
-        return io("/", { path: "https://namaste-node-js-rhsn.onrender.com/socket.io" })
-    }
-
-}
+};
